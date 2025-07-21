e1940a17be5f28082f7d02fcb4956cf3c6511336
Samuel Wei <samuelwwei@163.com>
2025-07-21 23:46:11 +1200

    delete space

--- internal/service/contract/contract_position.go
+++ internal/service/contract/contract_position.go
@@ -5,8 +5,7 @@ import (
 	"neocex/v2/global"
 	"neocex/v2/internal/models/contract"
 	"neocex/v2/internal/service/symbol"
-
-	"github.com/ericlagergren/decimal"
+	"github.com/ericlagergren/decimal" 
 )
 
 type ContractPositionService struct{}
@@ -26,6 +25,7 @@ func (c *ContractPositionService) GetUnrealizedProfitLoss(userId uint) (unrealiz
 		}
 
 	}
+	return
 }
 
 func (c *ContractPositionService) GetUnrealizedProfitLossSingle(symbolId int, quantity decimal.Big, openPrice decimal.Big, positionType contract.PositionType) (closePrice decimal.Big, unrealizedProfitLoss decimal.Big, err error) {
